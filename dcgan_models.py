53da7f2ee30b787671bc387c86134aa01211fc00
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2021-01-22 19:42:42 -0300

    create generator network

--- dcgan_models.py
+++ dcgan_models.py
@@ -13,7 +13,7 @@ ALPHA = 0.2
 class Discriminator(nn.Module):
     def __init__(self):
         """
-            Define the network architecture
+            Define the discriminator network architecture
             using a Sequential container
         """
         super(Discriminator,self).__init__()
@@ -40,6 +40,55 @@ class Discriminator(nn.Module):
             # Flatten result from 13x13x256
             nn.Linear(13*13*256,1),
             nn.Sigmoid()
+        )
+    
+    def forward(self,input):
+        return self.main(input)
+
+class View(nn.Module):
+    """
+        View implementation as a module for using it in nn.Sequential
+    """
+    def __init__(self,shape):
+        super(View,self).__init__()
+        self.shape = shape
+    
+    def forward(self,input):
+        return input.view(*self.shape)        
+
+class Generator(nn.Module):
+    def __init__(self):
+        """
+            Define the generator network architecture
+            using a Sequential container
+        """
+        super(Generator,self).__init__()
+        self.main = nn.Sequential(
+            #STEM GROUP
+            nn.Linear(100,18432),
+            nn.LeakyReLU(ALPHA,inplace=True),
+            View((-1,12,12,128)),
+            nn.ZeroPad2d((0,1,0,1)),
+            #Conv Group
+            #First Block
+            nn.ConvTranspose2d(128,128,4,stride=2,padding=0),
+            nn.Conv2d(128,128,3,stride=1,padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(ALPHA,inplace=True),
+            #Second Block
+            nn.ConvTranspose2d(128,64,4,stride=2,padding=0),
+            nn.Conv2d(64,64,3,stride=1,padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(ALPHA,inplace=True),
+            #Third Block
+            nn.ConvTranspose2d(64,32,4,stride=2,padding=0),
+            nn.Conv2d(32,32,3,stride=1,padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(ALPHA,inplace=True),
+            #Task Group
+            nn.ConvTranspose2d(32,1,3,stride=1,padding=1),
+            nn.Tanh(),
+            nn.UpsamplingNearest2d(size=(101,101))
         )
     
     def forward(self,input):
